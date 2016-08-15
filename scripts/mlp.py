5cb1e350b381d0a0554ee62e7e2ea1233ed9d876
gcoter <guillaume.coter@gmail.com>
2016-08-15 16:51:20 +0200

    All bugs fixed

--- scripts/mlp.py
+++ scripts/mlp.py
@@ -2,19 +2,107 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 import csv
+import os.path
+from six.moves import cPickle as pickle
 
 # === CONSTRUCT DATASETS ===
 image_size = 28
 num_labels = 10
+data_path = '../data/'
+pickle_file_path = data_path + 'train_MNIST.pickle'
+validation_proportion = 0.1
 
-def contruct_datasets():
-	return [], [], [], []
+def initialize_array(num_rows,image_size,num_labels):
+	dataset = np.ndarray((num_rows, image_size * image_size), dtype=np.float32)
+	labels = np.ndarray((num_rows, num_labels), dtype=np.int32)
+	return dataset, labels
+	
+def randomize(dataset,labels):
+	permutation = np.random.permutation(labels.shape[0])
+	shuffled_dataset = dataset[permutation,:]
+	shuffled_labels = labels[permutation]
+	print('Randomized dataset and labels')
+	return shuffled_dataset, shuffled_labels
+	
+def split_with_proportion(shuffled_dataset,shuffled_labels,validation_proportion):
+	validation_index = int(len(shuffled_dataset) * validation_proportion)
+	train_dataset = shuffled_dataset[validation_index:]
+	train_labels = shuffled_labels[validation_index:]
+	valid_dataset = shuffled_dataset[:validation_index]
+	valid_labels = shuffled_labels[:validation_index]
+	return train_dataset, train_labels, valid_dataset, valid_labels
+	
+def one_hot_vector(num_labels,label):
+	vector = np.zeros(num_labels, dtype=np.int32)
+	vector[int(label)] = 1
+	return vector
+	
+def save(pickle_file_path,train_dataset,train_labels,valid_dataset,valid_labels):
+	try:
+		f = open(pickle_file_path, 'wb')
+		save = {
+			'train_dataset': train_dataset,
+			'train_labels': train_labels,
+			'valid_dataset': valid_dataset,
+			'valid_labels': valid_labels,
+		}
+		pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+		f.close()
+		print('Datasets saved to ', pickle_file_path)
+	except Exception as e:
+		print('Unable to save data to', pickle_file_path, ':', e)
+		raise
+		
+def load(pickle_file_path):
+	with open(pickle_file_path, 'rb') as f:
+		save = pickle.load(f)
+		train_dataset = save['train_dataset']
+		train_labels = save['train_labels']
+		valid_dataset = save['valid_dataset']
+		valid_labels = save['valid_labels']
+		del save  # hint to help gc free up memory
+		print('Loaded datasets from ', pickle_file_path)
+	  
+	return train_dataset, train_labels, valid_dataset, valid_labels
 
-train_dataset, train_labels, valid_dataset, valid_labels = contruct_datasets()
+def construct_datasets(data_path,pickle_file_path,image_size,validation_proportion):	
+	with open(data_path + 'train.csv', 'rb') as csvfile:
+		print('Reading csv file...')
+		reader = csv.reader(csvfile, delimiter=',')
+		num_images = sum(1 for row in reader)
+		csvfile.seek(0)
+		print(num_images,'rows counted')
+		dataset, labels = initialize_array(num_images,image_size,num_labels)
+		print('Data set', dataset.shape, labels.shape)
+		id = 0
+		for row in reader:
+			# skip first row
+			if id > 0:
+				dataset[id-1] = row[1:]
+				labels[id-1] = one_hot_vector(num_labels,row[0])
+			id += 1
+		print(id, 'rows read')
+		shuffled_dataset, shuffled_labels = randomize(dataset, labels)
+		train_dataset, train_labels, valid_dataset, valid_labels = split_with_proportion(shuffled_dataset,shuffled_labels,validation_proportion)
+		print('Datasets constructed')
+		save(pickle_file_path,train_dataset,train_labels,valid_dataset,valid_labels)
+		return train_dataset, train_labels, valid_dataset, valid_labels
+		
+	return None, None, None, None
+	
+def get_datasets(data_path,pickle_file_path,image_size,validation_proportion):
+	if os.path.isfile(pickle_file_path):
+		return load(pickle_file_path)
+	else:
+		return construct_datasets(data_path,pickle_file_path,image_size,validation_proportion)
+
+train_dataset, train_labels, valid_dataset, valid_labels = get_datasets(data_path,pickle_file_path,image_size,validation_proportion)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
 
 # === HYPERPARAMETERS ===
-""" With those parameters, I get 86.7% of accuracy """
-network_shape = [image_size * image_size,600,300,num_labels]
+network_shape = [image_size * image_size,1024,num_labels]
+num_layers = len(network_shape)
 initial_learning_rate = 0.1
 decay_steps = 0
 decay_rate = 0
@@ -70,10 +158,14 @@ def accuracy(predictions, labels):
 	return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
 	
 # === TRAINING ===
+batch_size = 150
+num_epochs = 2
+num_steps = len(train_dataset)/batch_size * num_epochs
 old_valid_accuracy = None
 with tf.Session(graph=graph) as session:
 	tf.initialize_all_variables().run()
 	print("Initialized with shape ", str(network_shape))
+	print('Start training',num_epochs,'epochs (',num_steps,'steps) with batch size',batch_size)
 	for step in range(num_steps+1):
 		# Pick an offset within the training data, which has been randomized.
 		offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
@@ -92,11 +184,13 @@ with tf.Session(graph=graph) as session:
 			valid_accuracy = accuracy(valid_prediction, valid_labels)
 			print("Validation accuracy: %.1f%%" % valid_accuracy)
 			old_valid_accuracy = valid_accuracy
-			
+
+"""			
 # === TEST ===
 test_dataset = []
-test_prediction = session.run(prediction, feed_dict={tf_dataset : test_dataset, tf_labels : test_labels, keep_prob : 1.0})
+test_prediction = session.run(prediction, feed_dict={tf_dataset : test_dataset, keep_prob : 1.0})
 
 # === GENERATE SUBMISSION FILE ===
 def generate_submission_file(test_prediction):
 	print("TO DO")
+"""
