863cd9ceb68ae3a61c4f63a290124154e4d78e67
gcoter <guillaume.coter@gmail.com>
2016-08-28 21:25:28 +0200

    Use functions to construct tf model

--- scripts/conv.py
+++ scripts/conv.py
@@ -31,19 +31,19 @@ initial_learning_rate = 1e-4
 dropout_keep_prob = 0.5
 
 # === MODEL DEFINITION ===
-def weight_variable(shape):
+def new_weights(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
 
-def bias_variable(shape):
+def new_biases(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
   
 def conv2d(x, W):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def max_pool(x, k=2):
+  return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 def convolutions(x,image_size,patch_size,depths):
 	# Variables
@@ -90,48 +90,23 @@ def mlp(x,network_shape,keep_prob):
 
 	return logits_2
 
-
 graph = tf.Graph()
 with graph.as_default():
 	# Placeholders
 	tf_dataset = tf.placeholder(tf.float32, shape=[None, 784])
 	tf_labels = tf.placeholder(tf.float32, shape=[None, 10])
-
-	# First Convolutional Layer
-	W_conv1 = weight_variable([5, 5, 1, 32])
-	b_conv1 = bias_variable([32])
-	x_image = tf.reshape(tf_dataset, [-1,28,28,1])
-
-	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-	h_pool1 = max_pool_2x2(h_conv1)
-
-	# Second Convolutional Layer
-	W_conv2 = weight_variable([5, 5, 32, 64])
-	b_conv2 = bias_variable([64])
-
-	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-	h_pool2 = max_pool_2x2(h_conv2)
-
-	# Densely Connected Layer
-	W_fc1 = weight_variable([7 * 7 * 64, 1024])
-	b_fc1 = bias_variable([1024])
-
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-	# Dropout
+	# Dropout keep probability (set to 1.0 for validation and test)
 	keep_prob = tf.placeholder(tf.float32)
-	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+	
+	conv_output = convolutions(tf_dataset,image_size,patch_size,depths)
+	reshaped_conv_output = tf.reshape(conv_output, [-1, network_shape[0]])
+	mlp_output = mlp(reshaped_conv_output,network_shape,keep_prob)
 
-	# Readout Layer
-	W_fc2 = weight_variable([1024, 10])
-	b_fc2 = bias_variable([10])
-
-	prediction=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+	prediction=tf.nn.softmax(mlp_output)
 
 	# Train and Evaluate the Model
 	loss = tf.reduce_mean(-tf.reduce_sum(tf_labels * tf.log(tf.clip_by_value(prediction,1e-10,1.0)), reduction_indices=[1]))
-	optimizer = tf.train.AdamOptimizer(1e-4).minimize(loss)
+	optimizer = tf.train.AdamOptimizer(initial_learning_rate).minimize(loss)
 
 def accuracy(predictions, labels):
 	return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
